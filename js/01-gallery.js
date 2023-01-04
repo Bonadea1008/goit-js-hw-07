86b680dfb665911c031163d08d24c0d6fefa5548
Anna <100880lar@gmail.com>
2023-01-04 11:05:58 +0200

    Add js to tasks 01 and 02

---
+++ js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const picturesCardMarkup = createPicturesCard(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", picturesCardMarkup);
+
+function createPicturesCard(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onPictureClick);
+
+function onPictureClick(evt) {
+  evt.preventDefault();
+  window.addEventListener("keydown", onEscapePress);
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  function onEscapePress(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+console.log(galleryItems);
+
+//library
+const libraryScriptJS = document.createElement("script");
+libraryScriptJS.src =
+  "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+
+document.body.appendChild(libraryScriptJS);
+
+const libraryLinkCSS = document.createElement("link");
+libraryLinkCSS.rel = "stylesheet";
+libraryLinkCSS.href =
+  "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css";
+
+document.head.appendChild(libraryLinkCSS);
---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+const imageCardsMarkup = createImageCards(galleryItems);
+
+function createImageCards(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /> </a>`;
+    })
+    .join("");
+}
+
+galleryList.insertAdjacentHTML("afterbegin", imageCardsMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
